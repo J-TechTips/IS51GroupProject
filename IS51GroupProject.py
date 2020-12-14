27abd5de9d8a646d9792f1411c7d45d79355238a
J-TechTips <jtecktips@gmail.com>
2020-12-13 19:09:10 -0800

    added Structed English and Pseudocode that was taken out

--- IS51GroupProject.py
+++ IS51GroupProject.py
@@ -1,3 +1,51 @@
+"""
+Stuctured English
+
+We wish to make a small python script to simulate the access panal for information.
+To begin the user will be displayed a login screen. 
+If the user fails to login a security notificaiton appears.
+If the user logins in successfully then the information is then displayed.
+The information is about US naval battleships.
+Only three ships will be displayed from a list of ten.
+The list will be from a seprate txt document that is used as a dictionary.
+The dictionary will allow us to display the information to the user.
+"""
+
+"""
+Pseudocode
+
+import random, os, time, json (each in their own line)
+
+main()
+    time.sleep (delay time - act like a loading computer)
+    print(insert welcom text)
+    while 
+        enter user name
+        time sleep 
+        enter password
+
+        if 
+            time sleep
+            print (login good)
+            log in 
+            dictionary created 
+            break loop
+        else
+            print (security notified)
+            exit()
+
+login()
+    time sleep
+    print welcome statement for login 
+
+create dictionary()
+    open file
+    ships as list name
+    close file
+    display 3 random ships
+    print the 3 ships in each their own line
+"""
+
 import random
 import os
 import time 
